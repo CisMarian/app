957ec92651e624d30e69b9ee1423ca0752b45955
Marian Cis <cis-marian@wp.pl>
2023-06-19 17:57:29 +0200

    add jsondb extension

--- app/routers.py
+++ app/routers.py
@@ -1,6 +1,8 @@
 from .init import api
-from .endpoints import Notes, NotesList
+from .endpoints import Note, Notes, Users, Profile
 
 
-api.add_resource(Notes, '/notes/<int:note_id')
-api.add_resource(NotesList, '/notes')
+api.add_resource(Note, '/note/<int:note_id')
+api.add_resource(Notes, '/notes')
+api.add_resource(Users, '/users')
+api.add_resource(Profile, '/profile/<int:user_id')
