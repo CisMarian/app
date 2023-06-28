e35475b96a24659424215b4af3762040669d943c
Marian Cis <cis-marian@wp.pl>
2023-06-28 19:13:07 +0200

    logowanie

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import logging
 from flask import Flask
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.INFO)
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    log.info('Tutaj zdarzyło się coś')
+
 
 if __name__ == "__main__":
+    main()
     app.run()
